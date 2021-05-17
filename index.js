7ce14306adaca5d6a3ac45394d7235dc18d66105
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-05-17 14:38:57 +0900

    [Add] socketのイベント発行を追加

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    console.log(`message: ${msg}`);
+  });
 });
 
 server.listen(3000, () => {
