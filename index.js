d0b82307c2475bf1e80f28fdeb30513927407086
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-05-25 11:28:28 +0900

    [Finish] homework終了

--- index.js
+++ index.js
@@ -5,19 +5,59 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const userHash = {};
+const connectedUsers = {};
+let guestCount = 0; // 名前無しの数
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(`${__dirname}/index.html`);
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
   socket.on("disconnect", () => {
-    console.log("a user disconnected");
+    const msg = `${userHash[socket.id]}が退出しました`;
+    delete userHash[socket.id];
+    socket.broadcast.emit("chat message", msg); // 送ってきた本人以外に送信
+    io.emit("users count", userHash);
   });
 
   socket.on("chat message", (msg) => {
-    console.log(`message: ${msg}`);
-    io.emit("chat message", msg);
+    io.emit("chat message", `${userHash[socket.id]}: ${msg}`);
+  });
+
+  socket.on("private message", (data) => {
+    const from = data.from,
+      to = data.to,
+      message = data.message;
+    if (connectedUsers.hasOwnProperty(to)) {
+      connectedUsers[to].emit("chat message", `DM(${from}): ${message}`);
+    }
+  });
+
+  socket.on("enter room", (userName) => {
+    socket.userName = userName; // socketにset
+    guestCount++;
+    socket.userName =
+      userName == null || userName == "" ? `ゲスト${guestCount}` : userName;
+    connectedUsers[userName] = socket;
+
+    userHash[socket.id] = socket.userName;
+    socket.broadcast.emit("chat message", `${socket.userName}が入出しました`);
+    io.emit("users count", userHash);
+  });
+
+  let nowTyping = 0; // start typingイベントを受信した回数
+  socket.on("start typing", () => {
+    if (nowTyping <= 0) {
+      socket.broadcast.emit("start typing", userHash[socket.id]);
+    }
+    nowTyping++;
+    setTimeout(() => {
+      nowTyping--;
+      if (nowTyping <= 0) {
+        socket.broadcast.emit("stop typing");
+      }
+    }, 3000);
   });
 });
 
